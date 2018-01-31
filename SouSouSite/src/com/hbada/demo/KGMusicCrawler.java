4ef8a0e1de21541c370c5350555e00fda78ced9f
XIURongYao <honourx@qq.com>
2018-01-31 22:05:08 +0800

    KG更新

--- SouSouSite/src/com/hbada/demo/KGMusicCrawler.java
+++ SouSouSite/src/com/hbada/demo/KGMusicCrawler.java
@@ -21,29 +21,26 @@ import com.alibaba.fastjson.JSONObject;
 public class KGMusicCrawler {
 	
 	
-private static int count = 1;
+	private static int count = 1;
 	
 	public static void main(String[] args) {
-		String p = "1"; 		//下载的页数，每页20，不太可能超过2000首吧
+		String page = "1"; 		//下载的页数，每页20，不太可能超过2000首吧
 		String word = "曾轶可";       //你搜索的歌手或者歌曲名字
-		String f = "e:/music/"+word+"2/"; //下载到此位置
-		url1(word,Integer.parseInt(p));
+		url1(word,Integer.parseInt(page));
 		//downTaskList(f,word,Integer.parseInt(p));
 	}
 	
 	/**
-	 * url1的获取
+	 * 获取url1
 	 * @param word
-	 * @return
+	 * @param p
+	 * @return url2(songNames,songURL)
 	 */
 	@SuppressWarnings("static-access")
-	public static Map<String,List<String>> url1(String word,int p) {
-		//String url1="http://www.kugou.com/yy/html/search.html#searchType=song&searchKeyWord="+word;
-		//String url1 = "https://c.y.qq.com/soso/fcgi-bin/client_search_cp?&lossless=0&flag_qc=0&p="+p+"&n=20&w="+word.replaceAll("\\s*", "");
-		//String url1="http://songsearch.kugou.com/song_search_v2?callback=jQuery1124036408845400483436_1517036450326&keyword=%E5%91%A8%E6%9D%B0%E4%BC%A6&page=1&pagesize=30&userid=-1&clientver=&platform=WebFilter&tag=em&filter=2&iscorrection=1&privilege_filter=0&_=1517036450328";
-		String url1="http://songsearch.kugou.com/song_search_v2?callback=jQuery112400037371328988002883_1517032536185&keyword="+word+"&page="+p+"&pagesize=30&userid=-1&clientver=&platform=WebFilter&tag=em&filter=2&iscorrection=1&privilege_filter=0&_=1517032536187";
+	public static Map<String,List<String>> url1(String word,int page) {
+		String url1="http://songsearch.kugou.com/song_search_v2?callback=jQuery112400037371328988002883_1517032536185&keyword="+word+"&page="+page+"&pagesize=30&userid=-1&clientver=&platform=WebFilter&tag=em&filter=2&iscorrection=1&privilege_filter=0&_=1517032536187";
 		try {
-			//1.请求网页
+			//1.请求网页;
 			URL pageUrl = new URL(url1);
 			//2.下载网页
 			BufferedReader reader = new BufferedReader(new InputStreamReader(
@@ -60,173 +57,75 @@ private static int count = 1;
 			if(matcher.find()){
 				line = matcher.group();
 			}
-			//String linee = new String(line.getBytes("utf-8"),"gbk");
-			//System.out.println(line);
 			JSONObject jo = new JSONObject();
 			JSONObject joline = jo.parseObject(line);
 			JSONObject jldata = (JSONObject) joline.get("data");
-			//System.out.println(joline);
 			JSONArray jlist = jldata.getJSONArray("lists");
-			System.out.println(jlist);
+			List<String> songNames = new ArrayList<String>();
+			List<String> songURLs = new ArrayList<String>(); 
 			for (int i = 0; i < jlist.size(); i++) {
 				JSONObject songlist = jlist.getJSONObject(i);				
-			    String SongName = (String)songlist.get("SongName");				
-				String fileHash = (String) songlist.get("FileHash");			
+			    String songName = (String)songlist.get("SongName");	
+				String fileHash = (String) songlist.get("FileHash");
 				String albumID = (String) songlist.get("AlbumID");
-				
+				songNames.add(songName);
+				songURLs.add("http://www.kugou.com/song/#hash="+fileHash+"&album_id="+albumID);
 			}
 			
-			
-//		myos
-		
-		
-			/*List<Object> mids = null,songmids = null;
-			mids = new ArrayList<Object>();
-			songmids = new ArrayList<Object>();
-			List<String> songnames = null,singers = null,songname_hilights = null;
-			songnames = new ArrayList<String>();
-			singers = new ArrayList<String>();
-			songname_hilights = new ArrayList<String>();
-			
-			for(Object j : jldata_song_list){
-				mids.add(((JSONObject) j).get("media_mid"));
-				songmids.add(((JSONObject) j).get("songmid"));
-				songnames.add((String)((JSONObject) j).get("songname"));
-				songname_hilights.add((String)((JSONObject) j).get("albumname_hilight")==null?"无":(String)((JSONObject) j).get("albumname_hilight"));
-				singers.add((String)((JSONObject)((JSONArray)((JSONObject) j).get("singer")).get(0)).get("name"));
-			}
-			
-			return url2(mids,songmids,songnames,singers,songname_hilights);*/
+			return url2(songNames,songURLs);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
-	}}
+	}
+	
 	/**
-	 * url2的获取
-	 * @param mids
-	 * @param songmids
-	 * @param songnames
-	 * @param singers
-	 * @return
+	 * 获取url2
+	 * @param songNames
+	 * @param songURLs
+	 * @return 
 	 */
-/*//	@SuppressWarnings("static-access")
-//	public static Map<String, List<String>> url2(List<Object> mids,List<Object> songmids,List<String> songnames,List<String> singers,List<String> songname_hilights ){
-//		
-//		List<String> vkeys = new ArrayList<String>();
-//		
-//		for(int i=0;i<mids.size();i++){
-//			
-//			String url2 = "https://c.y.qq.com/base/fcgi-bin/fcg_music_express_mobile3.fcg?&jsonpCallback=MusicJsonCallback&cid=205361747"
-//					+ "&songmid="+songmids.get(i)+"&filename=C400"+mids.get(i)+".m4a&guid=6612300644";
-//			try {
-//				URL pageUrl = new URL(url2);
-//				BufferedReader reader = new BufferedReader(new InputStreamReader(
-//						pageUrl.openStream()));
-//				String line;
-//				StringBuffer pageBuffer = new StringBuffer();
-//				while ((line = reader.readLine()) != null) {
-//					pageBuffer.append(line);
-//				}
-//				
-//				JSONObject jo = new JSONObject();
-//				JSONObject joline = jo.parseObject(pageBuffer.toString());
-//				
-//				JSONObject jldata = (JSONObject) joline.get("data");
-//				
-//				if(((JSONArray) jldata.get("items")).size()>0){
-//					JSONObject jldata_items = (JSONObject)((JSONArray) jldata.get("items")).get(0);
-//					String vkey = (String) jldata_items.get("vkey");
-//					vkeys.add(vkey);
-//				}else{
-//					vkeys.add("");
-//				}
-//			
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//		}
-//		return url3(mids,vkeys,songnames,singers,songname_hilights);
-//	}
-//	
-//	/**
-//	 * url3的获取
-//	 * @param mids
-//	 * @param vkeys
-//	 * @return
-//	 */
-//	public static Map<String,List<String>> url3(List<Object> mids,List<String> vkeys,List<String> songnames,List<String> singers,List<String> songname_hilights){
-//		Map<String,List<String>> songMap = new HashMap<String,List<String>>();
-//		List<String> url3s = new ArrayList<String>();
-//		for(int i=0;i<mids.size();i++){
-//			String url3 = "http://dl.stream.qqmusic.qq.com/C400"+mids.get(i)+".m4a?vkey="+vkeys.get(i)+"&guid=6612300644&uin=0&fromtag=66";
-//			url3s.add(url3);
-//		}
-//		songMap.put("song_names", songnames);
-//		songMap.put("song_singers", singers);
-//		songMap.put("song_songname_hilights", songname_hilights);
-//		songMap.put("song_urls", url3s);
-//		return songMap;
-//	}
-//	
-//
-//	/**
-//	 * 下载单个music
-//	 * @param f
-//	 * @param n
-//	 * @param u
-//	 * @return
-//	 */
-//	public static boolean downMusic(String f,String n,String u){
-//		//下载音乐数据的方法
-//        try {  
-//        	URL url = new URL(u);  
-//            DataInputStream dataInputStream = new DataInputStream(url.openStream());  
-//            String imageName = n +".m4a";
-//            File file=new File(f); 
-//            if(!file.isDirectory()){  
-//                file.mkdirs();  
-//            }  
-//            FileOutputStream fileOutputStream = new FileOutputStream(new File(f + imageName.trim()));  
-//            byte[] buffer = new byte[1024];  
-//            int length;  
-//            while ((length = dataInputStream.read(buffer)) > 0) {  
-//                fileOutputStream.write(buffer, 0, length);  
-//            }  
-//            dataInputStream.close();  
-//            fileOutputStream.close();
-//            return true;
-//        } catch (Exception e) {
-//        	//e.printStackTrace();//有时下载会出错返回false
-//        }
-//        
-//		return false;  
-//	}
-//
-//	/**
-//	 * 开始下载任务
-//	 * @param f
-//	 * @param w
-//	 */
-//	public static void downTaskList(String f,String w,int p){
-//		long d1 = new Date().getTime();
-//		for(int j=1;j<p;j++){
-//			Map<String,List<String>> songMap = url1(w,j);
-//			List<String> song_names = (List<String>) songMap.get("song_names");
-//			List<String> song_singers = (List<String>) songMap.get("song_singers");
-//			List<String> urls = (List<String>) songMap.get("song_urls");
-//			for(int i=0;i<urls.size();i++){
-//				if(downMusic(f,song_names.get(i)+"-"+song_singers.get(i),urls.get(i))){
-//					System.out.println("已下载"+(count++)+"首");
-//				}else{
-//					System.out.println("第"+(count++)+"首下载失败");
-//				}
-//			}
-//			if(urls.size()<1){
-//				break;
-//			}
-//		}
-//		System.out.print("QQ音乐下载完成.");
-//		long d2 = new Date().getTime();
-//		System.out.println("用时："+(d2-d1)/1000+"秒");
-//	}
+	private static Map<String, List<String>> url2(List<String> songNames,List<String> songURLs){
+		
+		for(String songURL : songURLs){
+			System.out.println(songURL);
+			try {
+				
+				//1.请求网页;
+				URL pageUrl = new URL(songURL);
+				//2.下载网页
+				BufferedReader reader = new BufferedReader(new InputStreamReader(
+						pageUrl.openStream()));
+				String line;
+				//3.读取网页
+				StringBuffer pageBuffer = new StringBuffer();
+				while ((line = reader.readLine()) != null) {
+					pageBuffer.append(line+"\n");
+				}
+				
+				line = pageBuffer.toString();
+				System.out.println(line);
+				break;
+				/*Matcher matcher = Pattern.compile("(?<=\\().*(?=\\))").matcher(line);
+				if(matcher.find()){
+					line = matcher.group();
+				}*/
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			
+			
+		}
+		
+		
+		
+		return null;
+		
+		
+	} 
+	
+
+}
+	
